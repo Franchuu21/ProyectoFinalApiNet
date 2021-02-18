26fd2b9f48544495f7b2260f0d10b1e9cb3dab69
Guivi <gguivisdalsky@accessinformatica.com.ar>
2021-02-18 14:47:08 -0300

    primer commit

---
+++ ProyectoFinalApi/Controllers/ProductoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProyectoFinalApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductoController : ControllerBase
+    {
+        private readonly ILogger<ProductoController> _logger;
+        public ProductoController(ILogger<ProductoController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+        /// <summary>
+        /// Obtener todos los productos
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<Producto> Get()
+        {
+            
+        }
+    }
+}
---
+++ ProyectoFinalApi/Models/Producto.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ProyectoFinalApi.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public DateTime FechaVencimiento { get; set; }
+    }
+}
